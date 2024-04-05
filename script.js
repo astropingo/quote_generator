396d66ff1f0a28cc8f62b226394cd89ad4b1d21d
mvp <mvpetri@gmail.com>
2024-04-05 01:39:55 -0300

    Added button functionality

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
+const quoteConstainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
+
 // let apiQuotes = [];
 
 // Show New Quote
 function newQuote() {
     const quote = localQuotes[Math.floor(Math.random(localQuotes.length) * localQuotes.length)]
-    console.log(quote)
+    if (!quote.author) {
+        authorText.textContent = "Unknown";
+    } else {
+        authorText.textContent = quote.author;
+    }
+    // Check Quote length to determine styling
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;
 }
 
 // Get Quotes From API
@@ -20,4 +38,15 @@ function newQuote() {
 
 // getQuotes();
 
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+//Event Listeners
+newQuoteBtn.addEventListener("click", newQuote)
+twitterBtn.addEventListener("click", tweetQuote)
+
+// On Load
 newQuote();
