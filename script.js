f211d1cf32780beebddca29424246e7c47494eae
mvp <mvpetri@gmail.com>
2024-04-05 01:22:13 -0300

    Added quote files

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Hello, World!")
+// let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+    const quote = localQuotes[Math.floor(Math.random(localQuotes.length) * localQuotes.length)]
+    console.log(quote)
+}
+
+// Get Quotes From API
+// async function getQuotes() {
+//     const apiUrl = 'https://type.fit/api/quotes';
+//     try {
+//         const response = await fetch(apiUrl);
+//         apiQuotes = await response.json();
+//         newQuote();
+//     } catch (error) {
+//         // Catch Error Here
+//     }
+// }
+
+// getQuotes();
+
+newQuote();
